e61f3be57d937ab404cdd7a6ba5110bc0609cabb
DylanEHolland <salinson1138@gmail.com>
2020-10-05 18:43:47 -0500

    .

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
 from distutils.core import setup
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
+  name = 'pykgr',
+  packages = ['pykgr'],
+  version = '0.1',
+  description = 'A never root software builder, like nix for poor people',
+  author = 'Dylan Holland',
   author_email = 'salinson1138@gmail.com',
   url = 'https://github.com/DylanEHolland/pykgr',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/DylanEHolland/pykgr/archive/0.1.tar.gz',
   keywords = ['SOFTWARE',"PACKAGER"],
   install_requires=[ 
           'argparse',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha', # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: BSD-3-Clause',
     'Programming Language :: Python :: 3',
   ],
 )
