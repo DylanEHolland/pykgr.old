1518675746913445c15113632c83080f083f8b92
DylanEHolland <salinson1138@gmail.com>
2021-08-06 19:08:54 -0700

    Changed test name

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -2,9 +2,17 @@ import pykgr
 import os
 
 
-def test_environment():
-    pass
+class DontRunTestPackage(pykgr.Package):
+    file_url = "https://ftp.gnu.org/gnu/hello/hello-2.9.tar.gz"
+    file_name = "hello-2.9.tar.gz"
+    name = "hello"
+    version = "2.9"
+
+
+def test_build():
+    env = pykgr.Environment()
+    env.build_package(DontRunTestPackage)
 
 
 if __name__ == "__main__":
-    test_environment()
+    test_build()
